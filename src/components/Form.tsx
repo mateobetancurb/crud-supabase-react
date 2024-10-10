5de8b4c51d9744db211597047a97c19845a73994
Mateo Betancur B <mateobetancurb@gmail.com>
2024-10-10 14:28:28 -0500

    Refactor Form component to improve accessibility and styling

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,8 @@
 export function Form() {
 	return (
-		<form action="">
-			<label htmlFor="">test</label>
-			<input type="text" name="" id="" />
+		<form action="" className="flex gap-3">
+			<label htmlFor="add">Add your task</label>
+			<input type="text" id="add" />
 		</form>
 	);
 }
