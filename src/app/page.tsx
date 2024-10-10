7d9db3cdb0e322a6c24ba9ca813b033d3e13ac19
Mateo Betancur B <mateobetancurb@gmail.com>
2024-10-10 16:04:26 -0500

    feature: todo list component to show user task

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import { Header, Form, TodoList, TodosCounter, Footer } from "@/components";
 
+const todoList = [
+	{ id: 1, description: "Task 1" },
+	{ id: 2, description: "Task 2" },
+];
+
 export default function App() {
 	return (
 		<main className="mt-10 bg-gray-900 p-10 w-3/4 md:w-1/2 mx-auto border border-gray-400 rounded-lg">
 			<Header />
 			<Form />
-			<TodoList description={"study programming"} />
+			<TodoList list={todoList} />
 			<TodosCounter />
 			<Footer />
 		</main>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,8 @@
 export function Form() {
 	return (
-		<form action="" className="flex gap-3">
+		<form action="" className="flex items-center gap-3 mb-5">
 			<label htmlFor="add">Add your task</label>
-			<input type="text" id="add" />
+			<input type="text" id="add" className="py-2 px-3 rounded-md text-black" />
 		</form>
 	);
 }
