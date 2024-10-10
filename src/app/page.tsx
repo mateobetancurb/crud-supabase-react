e57128519a3cdd17256e84260334d1beeb303f7c
Mateo Betancur B <mateobetancurb@gmail.com>
2024-10-10 14:28:43 -0500

    Refactor page component to include TodoList and Footer components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import { Header, Form, TodosCounter } from "@/components";
+import { Header, Form, TodoList, TodosCounter, Footer } from "@/components";
 
 export default function App() {
 	return (
-		<main className="mt-10 bg-gray-900 p-10 w-3/4 md:w-1/2 lg:w-1/4 mx-auto border border-gray-400 rounded-lg">
+		<main className="mt-10 bg-gray-900 p-10 w-3/4 md:w-1/2 mx-auto border border-gray-400 rounded-lg">
 			<Header />
 			<Form />
+			<TodoList description={"study programming"} />
 			<TodosCounter />
+			<Footer />
 		</main>
 	);
 }
