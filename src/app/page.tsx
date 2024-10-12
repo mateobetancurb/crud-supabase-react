c94a65ab6532d82fd0d16cf105cea0ddfefaea99
Mateo Betancur B <mateobetancurb@gmail.com>
2024-10-12 18:48:18 -0500

    refactor: Update App component to fetch and display todos from the server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
 import { Header, Form, TodoList, TodosCounter, Footer } from "@/components";
+import { getAllTodos } from "@/actions";
 
 const todoList = [
 	{ id: 1, description: "Task 1" },
 	{ id: 2, description: "Task 2" },
 ];
 
-export default function App() {
+export default async function App() {
+	const todos = await getAllTodos();
+
 	return (
 		<main className="mt-10 bg-gray-900 p-10 w-3/4 md:w-1/2 mx-auto border border-gray-400 rounded-lg">
 			<Header />
 			<Form />
-			<TodoList list={todoList} />
-			<TodosCounter />
+			<TodoList todos={todos} />
+			<TodosCounter totalTodos={todos.length} />
 			<Footer />
 		</main>
 	);
