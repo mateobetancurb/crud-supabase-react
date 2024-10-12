93ce88166dc5cc38be50316d55938ceeb5f1fb42
Mateo Betancur B <mateobetancurb@gmail.com>
2024-10-12 14:25:07 -0500

    refactor: Update footer message to mention Supabase usage

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
 	return (
 		<footer className="text-xs font-thin pt-10">
-			But this time was build in Supabase instead of using localStorage 😂
+			...but this time was build in Supabase instead of using localStorage 😂
 		</footer>
 	);
 }
