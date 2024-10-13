cc1da7437c0a1cae76e151a7e891a16f033dd23d
Mateo Betancur B <mateobetancurb@gmail.com>
2024-10-13 10:41:13 -0500

    refactor: Update actions/index.ts to include deleteTodoById function

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -21,23 +21,37 @@ export async function getAllTodos() {
 
 export async function createTodo(message: string, assing_to: string) {
 	try {
-		const response = await fetch(
-			`${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/todos`,
+		await fetch(`${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/todos`, {
+			method: "POST",
+			headers: {
+				apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
+				Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
+				"Content-Type": "application/json",
+				Prefer: "return=null",
+			},
+			body: JSON.stringify({
+				message,
+				assing_to,
+			}),
+		});
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+export async function deleteTodoById(id: string | number) {
+	try {
+		await fetch(
+			`${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/todos?id=eq.${id}`,
 			{
-				method: "POST",
+				method: "DELETE",
 				headers: {
 					apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
 					Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
-					"Content-Type": "application/json",
-					Prefer: "return=null",
 				},
-				body: JSON.stringify({
-					message,
-					assing_to,
-				}),
 			}
 		);
 	} catch (error) {
-		console.log(error);
+		console.log();
 	}
 }
