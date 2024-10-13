daffc2e203dcef0c187bebc668620063360a6264
Mateo Betancur B <mateobetancurb@gmail.com>
2024-10-13 00:00:15 -0500

    feat: Add createTodo function to create a new todo

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -18,3 +18,26 @@ export async function getAllTodos() {
 		console.log(error);
 	}
 }
+
+export async function createTodo(message: string, assing_to: string) {
+	try {
+		const response = await fetch(
+			`${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/todos`,
+			{
+				method: "POST",
+				headers: {
+					apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "",
+					Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
+					"Content-Type": "application/json",
+					Prefer: "return=null",
+				},
+				body: JSON.stringify({
+					message,
+					assing_to,
+				}),
+			}
+		);
+	} catch (error) {
+		console.log(error);
+	}
+}
