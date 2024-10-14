b18891427933c32f1cbcd9f57e1ead802ff8cd50
Mateo Betancur B <mateobetancurb@gmail.com>
2024-10-14 10:50:32 -0500

    Add API functions for CRUD operations on todos

---
+++ src/api/index.ts
@@ -0,0 +1,76 @@
+export async function getAllTodos() {
+	try {
+		const response = await fetch(
+			`${import.meta.env.VITE_SUPABASE_URL}/rest/v1/todos?select=*`,
+			{
+				headers: {
+					apikey: import.meta.env.VITE_SUPABASE_ANON_KEY || "",
+					Authorization: `Bearer ${import.meta.env.VITE_SUPABASE_ANON_KEY}`,
+				},
+			}
+		);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+export async function createTodo(message: string, assing_to: string) {
+	try {
+		await fetch(`${import.meta.env.VITE_SUPABASE_URL}/rest/v1/todos`, {
+			method: "POST",
+			headers: {
+				apikey: import.meta.env.VITE_SUPABASE_ANON_KEY || "",
+				Authorization: `Bearer ${import.meta.env.VITE_SUPABASE_ANON_KEY}`,
+				"Content-Type": "application/json",
+				Prefer: "return=null",
+			},
+			body: JSON.stringify({
+				message,
+				assing_to,
+			}),
+		});
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+export async function deleteTodoById(id: string | number) {
+	try {
+		await fetch(
+			`${import.meta.env.VITE_SUPABASE_URL}/rest/v1/todos?id=eq.${id}`,
+			{
+				method: "DELETE",
+				headers: {
+					apikey: import.meta.env.VITE_SUPABASE_ANON_KEY || "",
+					Authorization: `Bearer ${import.meta.env.VITE_SUPABASE_ANON_KEY}`,
+				},
+			}
+		);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+export async function updateTodoById(id: string | number, message: string) {
+	try {
+		await fetch(
+			`${import.meta.env.VITE_SUPABASE_URL}/rest/v1/todos?id=eq.${id}`,
+			{
+				method: "PATCH",
+				headers: {
+					apikey: import.meta.env.VITE_SUPABASE_ANON_KEY || "",
+					Authorization: `Bearer ${import.meta.env.VITE_SUPABASE_ANON_KEY}`,
+					"Content-Type": "application/json",
+					Prefer: "return=null",
+				},
+				body: JSON.stringify({
+					message,
+				}),
+			}
+		);
+	} catch (error) {
+		console.log(error);
+	}
+}
