9f4a2a0f66ab53e349ba5d09d1eafbd8b3ed24b1
Mateo Betancur B <mateobetancurb@gmail.com>
2024-10-13 22:33:27 -0500

    refactor: typescript interfaces were moved to its own file

---
+++ src/interfaces/index.ts
@@ -0,0 +1,19 @@
+export interface TodoItem {
+	id: string | number;
+	message: string;
+	assing_to: string;
+	created_at: string;
+}
+
+export interface Todos {
+	todos: TodoItem[];
+}
+
+export interface TotalTodos {
+	totalTodos?: number;
+}
+
+export interface TodoToDelete {
+	id: number | string;
+	message: string;
+}
