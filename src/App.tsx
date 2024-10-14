a58e2c1a370cb289177966488b0b441db4e5c375
Mateo Betancur B <mateobetancurb@gmail.com>
2024-10-14 12:11:05 -0500

    Add ErrorBoundary component to handle uncaught errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-function App() {
+import { useState, useEffect } from "react";
+import { getAllTodos } from "@/api";
+import { Header } from "@/components/Header.tsx";
+import { Form } from "@/components/Form.tsx";
+import { TodoList } from "@/components/TodoList";
+import { TodosCounter } from "@/components/TodosCounter";
+import { Footer } from "@/components/Footer";
+import { ErrorBoundary } from "@/ErrorBoundary";
+
+export default function App() {
+	const [todos, setTodos] = useState([]);
+
+	async function getTodos() {
+		const { todos } = await getAllTodos();
+		setTodos(todos);
+	}
+
+	useEffect(() => {
+		getTodos();
+	}, []);
+
 	return (
-		<>
-			<p>hello world!</p>
-		</>
+		<ErrorBoundary>
+			<main className="mt-10 bg-gray-900 p-10 w-3/4 md:w-1/2 mx-auto border border-gray-400 rounded-lg">
+				<Header />
+				<Form />
+				<TodoList todos={todos} />
+				<TodosCounter />
+				<Footer />
+			</main>
+		</ErrorBoundary>
 	);
 }
-
-export default App;
---
+++ src/ErrorBoundary.tsx
@@ -0,0 +1,37 @@
+import { Component, ErrorInfo, ReactNode } from "react";
+
+interface Props {
+	children?: ReactNode;
+}
+
+interface State {
+	hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+	public state: State = {
+		hasError: false,
+	};
+
+	public static getDerivedStateFromError(): State {
+		return { hasError: true };
+	}
+
+	public componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+		console.error("Uncaught error:", error, errorInfo);
+	}
+
+	public render() {
+		if (this.state.hasError) {
+			return (
+				<h1 className="text-white text-center mt-12">
+					⚠️ Something went wrong
+				</h1>
+			);
+		}
+
+		return this.props.children;
+	}
+}
+
+export { ErrorBoundary };
