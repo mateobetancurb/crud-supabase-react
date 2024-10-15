99a80227d64b08a21206e349f6d36679e015318f
Mateo Betancur B <mateobetancurb@gmail.com>
2024-10-15 00:06:48 -0500

    chore: context config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { Toaster } from "@/components/ui/sonner";
+import { AppProvider } from "@/context/AppProvider";
 import { getAllTodos } from "@/api";
 import { Header } from "@/components/Header.tsx";
 import { Form } from "@/components/Form.tsx";
@@ -6,7 +8,6 @@ import { TodoList } from "@/components/TodoList";
 import { TodosCounter } from "@/components/TodosCounter";
 import { Footer } from "@/components/Footer";
 import { ErrorBoundary } from "@/ErrorBoundary";
-import { Toaster } from "@/components/ui/sonner";
 
 export default function App() {
 	const [todos, setTodos] = useState([]);
@@ -23,29 +24,31 @@ export default function App() {
 
 	return (
 		<ErrorBoundary>
-			<main className="mt-10 bg-gray-900 p-10 w-3/4 md:w-1/2 mx-auto border border-gray-400 rounded-lg">
-				<Header />
-				<Form />
-				<TodoList todos={todos} />
-				<TodosCounter totalTodos={todos.length} />
-				<Footer />
-				<Toaster
-					position="top-right"
-					theme="light"
-					richColors
-					toastOptions={{
-						classNames: {
-							toast:
-								"shadow-lg rounded-lg flex items-center p-4 text-xs gap-1.5",
-							error: "[&>button]:!bg-red-600",
-							info: "[&>button]:!bg-blue-600",
-							success: "[&>button]:!bg-green-600",
-							warning: "[&>button]:!bg-yellow-600",
-						},
-					}}
-				/>
-				{/* <Sonner theme="light" richColors /> */}
-			</main>
+			<AppProvider>
+				<main className="mt-10 bg-gray-900 p-10 w-3/4 md:w-1/2 mx-auto border border-gray-400 rounded-lg">
+					<Header />
+					<Form />
+					<TodoList todos={todos} />
+					<TodosCounter totalTodos={todos.length} />
+					<Footer />
+					<Toaster
+						position="top-right"
+						theme="light"
+						richColors
+						toastOptions={{
+							classNames: {
+								toast:
+									"shadow-lg rounded-lg flex items-center p-4 text-xs gap-1.5",
+								error: "[&>button]:!bg-red-600",
+								info: "[&>button]:!bg-blue-600",
+								success: "[&>button]:!bg-green-600",
+								warning: "[&>button]:!bg-yellow-600",
+							},
+						}}
+					/>
+					{/* <Sonner theme="light" richColors /> */}
+				</main>
+			</AppProvider>
 		</ErrorBoundary>
 	);
 }
