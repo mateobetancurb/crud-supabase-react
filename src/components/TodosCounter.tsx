4701136007fa49e170c22daf70652ff9995acd92
Mateo <mateobetancurb@gmail.com>
2024-11-06 09:37:27 -0500

    Update TodosCounter to display message when todo list is empty

--- src/components/TodosCounter.tsx
+++ src/components/TodosCounter.tsx
@@ -4,8 +4,14 @@ function TodosCounter() {
 	const { todos } = useAppContext();
 	return (
 		<footer className="flex items-center gap-3 text-white font-bold">
-			<p>Total tasks:</p>
-			<p>{todos.length}</p>
+			{todos.length > 0 ? (
+				<>
+					<p>Total tasks:</p>
+					<p>{todos.length}</p>
+				</>
+			) : (
+				<p className="mx-auto">Your todo list are empty</p>
+			)}
 		</footer>
 	);
 }
