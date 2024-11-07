995a35709899bb884015e959ea2b0b5300662fa7
Mateo <mateobetancurb@gmail.com>
2024-11-07 14:22:28 -0500

    Add loading state to TodoList component for improved user experience

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -3,21 +3,26 @@ import { UpdateItem } from "@/components/UpdateItem";
 import { useAppContext } from "@/hooks/useAppContext";
 
 function TodoList() {
-	const { todos } = useAppContext();
+	const { todos, isLoadingTodos } = useAppContext();
+
 	return (
 		<section>
-			{todos.map((item) => (
-				<div
-					key={item.id}
-					className="flex justify-between mb-5 hover:bg-slate-500 hover:rounded-md transition-all p-2"
-				>
-					<p className="text-white">{item.message}</p>
-					<div className="flex gap-5 items-center">
-						<UpdateItem />
-						<DialogToDeleteItem message={item.message} id={item.id} />
+			{isLoadingTodos ? (
+				<p className="text-white text-center mb-10">Loading...</p>
+			) : (
+				todos.map((item) => (
+					<div
+						key={item.id}
+						className="flex justify-between mb-5 hover:bg-slate-500 hover:rounded-md transition-all p-2"
+					>
+						<p className="text-white">{item.message}</p>
+						<div className="flex gap-5 items-center">
+							<UpdateItem />
+							<DialogToDeleteItem message={item.message} id={item.id} />
+						</div>
 					</div>
-				</div>
-			))}
+				))
+			)}
 		</section>
 	);
 }
