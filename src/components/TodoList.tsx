c4799a7ea2a6f87efc0e0e3fd290fde17592424e
Mateo <mateobetancurb@gmail.com>
2024-11-26 11:42:10 -0500

    Refactor TodoList component to separate completed and pending todos into distinct sections

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -12,53 +12,57 @@ function TodoList() {
 	return (
 		<section>
 			<h2 className="text-lg text-white font-bold">Pending</h2>
-			{isLoadingTodos ? (
-				<p className="text-white text-center mb-10">Loading...</p>
-			) : (
-				pendingTodos.map((item) => (
-					<div
-						key={item.id}
-						className="flex mb-5 hover:bg-slate-500 hover:rounded-md transition-all p-2"
-					>
-						<form className="flex items-center mr-5">
-							<input
-								onChange={() => updateTodoByStatus(item.id, item.is_completed)}
-								type="checkbox"
-								className="w-4 h-4"
-								checked={item.is_completed}
-							/>
-						</form>
-						<div className="flex justify-between w-full">
-							<p
-								className={`${
-									item.is_completed === true
-										? "line-through text-gray-400"
-										: "text-white"
-								}`}
-							>
-								{item.message}
-							</p>
-							<div className="flex gap-5 items-center">
-								{item.is_completed ? null : (
-									<UpdateItem
-										initialMessage={item.message}
-										onUpdate={(newMessage) => updateTodo(item.id, newMessage)}
-									/>
-								)}
-								<DialogToDeleteItem message={item.message} id={item.id} />
+			<div className="mb-10">
+				{isLoadingTodos ? (
+					<p className="text-white text-center mb-10">Loading...</p>
+				) : (
+					pendingTodos.map((item) => (
+						<div
+							key={item.id}
+							className="flex mb-5 hover:bg-slate-500 hover:rounded-md transition-all p-2"
+						>
+							<form className="flex items-center mr-5">
+								<input
+									onChange={() =>
+										updateTodoByStatus(item.id, item.is_completed)
+									}
+									type="checkbox"
+									className="w-4 h-4"
+									checked={item.is_completed}
+								/>
+							</form>
+							<div className="flex justify-between w-full">
+								<p
+									className={`${
+										item.is_completed === true
+											? "line-through text-gray-400"
+											: "text-white"
+									}`}
+								>
+									{item.message}
+								</p>
+								<div className="flex gap-5 items-center">
+									{item.is_completed ? null : (
+										<UpdateItem
+											initialMessage={item.message}
+											onUpdate={(newMessage) => updateTodo(item.id, newMessage)}
+										/>
+									)}
+									<DialogToDeleteItem message={item.message} id={item.id} />
+								</div>
 							</div>
 						</div>
-					</div>
-				))
-			)}
-			<section>
+					))
+				)}
+			</div>
+			<div className="mb-10">
 				<h2 className="text-lg text-white font-bold">Completed</h2>
 				{completedTodos.map((item) => (
-					<li key={item.id} className="text-white my-5">
+					<li key={item.id} className="text-white">
 						{item.message}
 					</li>
 				))}
-			</section>
+			</div>
 		</section>
 	);
 }
